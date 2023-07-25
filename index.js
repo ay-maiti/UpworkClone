66edfd02f0f024862cb24a26d079dc1dd6890bf0
Ayan Maiti <ayanmaiti@outlook.com>
2023-07-25 23:48:10 +0530

    Closes #1. Added signup, login,  jwt and connected Mongodb

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require("express")
 const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose")
 const app = express()
 
 app.use(express.json())
 
+const mongodb_URI = "mongodb+srv://upwork:upwork@cluster0.fsn5iln.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(mongodb_URI)
+.then(()=>{
+    console.log("connected to mongodb.")
+})
+
 const prof_users = []
 const comp_users = []
 const SECRET_KEY = "JOSH"
+
+const userSchema = new mongoose.Schema(
+    {
+        firstname: {type: String, required: true},
+        lastname: {type: String, required:true},
+        email: {type: String, required:true},
+        password: {type: String, required:true},
+        country: {type: String, required:true},
+    }
+)
+
+const prof_user_db = mongoose.model('ProfUsers', userSchema)
+const comp_user_db = mongoose.model('CompUsers', userSchema)
 
 const authenticateJwt = (req, res, next)=>{
     const authHeader = req.headers.authorization
@@ -32,16 +53,15 @@ const authenticateJwt = (req, res, next)=>{
    Input: { firstname: 'Ayan', lastname:'Maiti', email: 'ayanmaiti@outlook.com', password: 'password', country: 'india' }
    Output: { message: 'Company created successfully' } 
 */
-app.post('/comp/signup', (req, res)=>{
-    const user_email = comp_users.find(p=>p.email===req.body.email)
+app.post('/comp/signup', async (req, res)=>{
+    const user_email = await comp_user_db.findOne({email:req.body.email});//comp_users.find(p=>p.email===req.body.email)
     if(user_email){
         res.status(403).json({'message':'User already exists'})
     }
     else{
-        const {firstname, lastname, email, password, country} = req.body;
-        comp_users.push(req.body)
-        console.log('email: '+req.body.email)
-        const token = jwt.sign({email, role:'comp'}, SECRET_KEY, {expiresIn:'1h'})
+        const new_comp = new comp_user_db(req.body)
+        new_comp.save()
+        const token = jwt.sign({email: req.body.email, role:'comp'}, SECRET_KEY, {expiresIn:'1h'})
         res.status(201).json({message:'User created successfully', token})
     }
 })
@@ -52,25 +72,50 @@ app.post('/comp/signup', (req, res)=>{
    Input: { firstname: 'Ayan', lastname:'Maiti', email: 'ayanmaiti@outlook.com', password: 'password', country: 'india' }
    Output: { message: 'Company created successfully' } 
 */
-app.post('/prof/signup', (req, res)=>{
-    const user_email = prof_users.find(p=>p.email===req.body.email)
+app.post('/prof/signup', async (req, res)=>{
+    const user_email = await prof_user_db.findOne({email: req.body.email})
     if(user_email){
         res.status(403).json({'message':'User already exists'})
     }
     else{
-        const {firstname, lastname, email, password, country} = req.body;
-        prof_users.push(req.body)
-        const token = jwt.sign({email, role:'prof'}, SECRET_KEY, {expiresIn:'1h'})
+        const new_prof = new prof_user_db(req.body)
+        new_prof.save()
+        const token = jwt.sign({email: req.body.email, role:'prof'}, SECRET_KEY, {expiresIn:'1h'})
         res.status(201).json({message:'User created successfully', token})
     }
 
 })
 
+/** - POST /comp/login
+   Description: Login to company account.
+   Input: {email: 'ayanmaiti@outlook.com', password: 'password'}
+   Output: { message: 'Logged in successfully' } 
+*/
+
+app.post('/comp/login', async(req, res)=>{
+    const {email, password} = req.headers
+    const user = await comp_user_db.findOne({email, password})
+    if(user){
+        const token = jwt.sign({email, role:'company'}, SECRET_KEY, {expiresIn: '1h'})
+        return res.status(200).json({message:'Logged in successfully', token})
+    }
+    res.sendStatus(403)
+})
 /** - POST /prof/login
    Description: Login to professional account.
-   Input: {email: 'ayanmaiti@outlook.com', password: 'password', country: 'india' }
-   Output: { message: 'Company created successfully' } 
+   Input: {email: 'ayanmaiti@outlook.com', password: 'password'}
+   Output: { message: 'Logged in successfully' } 
 */
+app.post('/prof/login', async(req, res)=>{
+    const {email, password} = req.headers
+    const user = await prof_user_db.findOne({email, password})
+    if(user){
+        const token = jwt.sign({email, role:'prof'}, SECRET_KEY, {expiresIn:'1h'})
+        return res.status(200).json({message:'Logged in successfully', token})
+    }
+    res.sendStatus(403)
+})
+
 app.post('/prof/view', authenticateJwt, (req, res)=>{
     res.sendStatus(201);
 })
