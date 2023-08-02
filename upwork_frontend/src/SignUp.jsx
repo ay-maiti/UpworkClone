5fed1eb910d488e3e1b07f19dd4bd11710ca93d5
Ayan Maiti <ayanmaiti@outlook.com>
2023-08-03 01:42:42 +0530

    issue #2: Added feature to select role before signup

--- upwork_frontend/src/SignUp.jsx
+++ upwork_frontend/src/SignUp.jsx
@@ -1,9 +1,49 @@
+import React from "react"
 import './SignUp.css'
 
 export default function SignUp() {
+    const [role, setRole] = React.useState("");
+    const [submit, setSubmit] = React.useState(false);
+    const client_btn_style = {
+
+        backgroundColor: role === "c" ? "lightgreen" : "white",
+        border: role === "c" ? "2px solid green" : "1px solid lightgray"
+    }
+    const freelancer_btn_style = {
+        backgroundColor: role === "f" ? "lightgreen" : "white",
+        border: role === "f" ? "2px solid green" : "1px solid lightgray"
+    }
+
+    function selectRole(user) {
+        setRole(user);
+    }
+
+    function confirmRole() {
+        setSubmit(true);
+    }
+
+    if (!submit) {
+        return (
+            <div className="main-box">
+                <h1 className="form-title">Join as a client or freelancer</h1>
+                <form className="role-form">
+                    <div className="role-btn" style={client_btn_style} onClick={() => { selectRole("c") }}>
+                        <h3>I'm a client, hiring for a project</h3>
+                    </div>
+                    <div className="role-btn" style={freelancer_btn_style} onClick={() => { selectRole("f") }}>
+                        <h3>I'm a freelancer, looking for work</h3>
+                    </div>
+                </form>
+                <button className="submit-role-btn" onClick={confirmRole} disabled={!role}>Create Account</button>
+                <p className='login-text'>Already have an account? <span><a href="/login">Log In</a></span></p>
+            </div>
+        )
+    }
+
+    const form_title = role === "c" ? "Sign up to hire talent" : "Sign up to find work you love";
     return (
         <div className="main-box">
-            <h1>Sign up to hire talent</h1>
+            <h1>{form_title}</h1>
             <form className="signup-form">
                 <input className='name' placeholder="First Name" />
                 <input className='name' placeholder="Last Name" />
